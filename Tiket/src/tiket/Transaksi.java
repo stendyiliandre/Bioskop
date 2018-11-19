f28d99f4e5b71efcbee3fe542664e2319a3aef0f
williamjohann21 <44402686+williamjohann21@users.noreply.github.com>
2018-11-19 21:35:07 +0700

    Add files via upload

---
+++ Tiket/src/tiket/Transaksi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiket;
+
+/**
+ *
+ * @author William Johann
+ */
+public class Transaksi {
+    public int id_jadwal;
+    public String kursi;
+
+    public int getId_jadwal() {
+        return id_jadwal;
+    }
+
+    public void setId_jadwal(int id_jadwal) {
+        this.id_jadwal = id_jadwal;
+    }
+
+    public String getKursi() {
+        return kursi;
+    }
+
+    public void setKursi(String kursi) {
+        this.kursi = kursi;
+    }
+}
