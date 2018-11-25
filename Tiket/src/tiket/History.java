b9684ca15cd0864d9ca579859c82b2c76b23029a
williamjohann21 <44402686+williamjohann21@users.noreply.github.com>
2018-11-25 23:38:35 +0700

    Add files via upload

---
+++ Tiket/src/tiket/History.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiket;
+
+import java.util.Date;
+
+/**
+ *
+ * @author William Johann
+ */
+public class History {
+    String id_user;
+    String kursi;
+    String studio;
+    String judul;
+    String tanggal;
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getKursi() {
+        return kursi;
+    }
+
+    public void setKursi(String kursi) {
+        this.kursi = kursi;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+}
