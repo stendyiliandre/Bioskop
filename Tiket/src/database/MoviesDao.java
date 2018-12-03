857cd97380aeba166434497644437fdb82f98d3e
kevinsuwanda <44392458+kevinsuwanda@users.noreply.github.com>
2018-12-03 23:05:35 +0700

    Add files via upload

--- Tiket/src/database/MoviesDao.java
+++ Tiket/src/database/MoviesDao.java
@@ -20,17 +20,30 @@ public class MoviesDao {
     public static void insertData(Movies m){
         ConnectionManager conMan = new ConnectionManager();
         Connection con = conMan.logOn();
+        String query = "insert into movies(judul,genre,durasi,directory) values (?,?,?,?)";
         try {
-            String query = "Insert into Movies(id_movie,judul,durasi,genre,directory)"
-                    + "value (?,?,?,?,?)";
-            System.out.println(query);
             PreparedStatement st= con.prepareStatement(query);
-            st.setString(1, Integer.toString(m.getId()));
-            st.setString(2, m.getJudul());
+            st.setString(1, m.getJudul());
+            st.setString(2, m.getGenre());
             st.setString(3, m.getDurasi());
-            st.setString(4, m.getGenre());
-            st.setString(5, m.getDirectory());
+            st.setString(4, m.getDirectory());
             
+            st.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace (System.err);
+        }
+        conMan.logOff();
+    }
+    
+    public static void deleteData(String showInputDialog){
+        ConnectionManager conMan = new ConnectionManager();
+        Connection con = conMan.logOn();
+        String query = "delete from movies"
+                + " WHERE id_movie = " +  showInputDialog;
+        try {
+            Statement st = con.createStatement();
+            
+            st.execute(query);
         } catch (SQLException ex) {
             ex.printStackTrace (System.err);
         }
@@ -47,7 +60,6 @@ public class MoviesDao {
             ResultSet rs = st.executeQuery(query);
             while(rs.next()){
                 Movies m = new Movies();
-                m.setId(Integer.parseInt(rs.getString("id_movie")));
                 m.setJudul(rs.getString("judul"));
                 m.setDurasi(rs.getString("durasi"));
                 m.setGenre(rs.getString("genre"));
@@ -60,4 +72,5 @@ public class MoviesDao {
         conMan.logOff();
         return listMovies;
     }
+
 }
